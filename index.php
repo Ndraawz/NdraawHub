da5494e96d75b64ce7a5633a049470345fa20ba8
Ndraawz <wolfykuns15@gmail.com>
2025-07-10 00:03:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<?php
-$ua = $_SERVER['HTTP_USER_AGENT'];
-$path = $_SERVER['REQUEST_URI'];
-$filename = ltrim($path, "/");
-
-if (strpos($ua, "Roblox") !== false || strpos($ua, "curl") !== false) {
-    if (file_exists($filename)) {
-        header("Content-Type: text/plain");
-        readfile($filename);
-    } else {
-        echo "-- Script not found!";
-    }
-} else {
-    echo "-- Access Denied";
-}
-?>
+Ioad = function(id)
+    local url = "https://script-auth.pages.dev/data/" .. id .. ".lua"
+    local success, result = pcall(function()
+        return game:HttpGet(url)
+    end)
+    if success then
+        loadstring(result)()
+    else
+        warn("Gagal load:", id)
+    end
+end
